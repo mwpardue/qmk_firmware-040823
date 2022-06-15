5df6e3f03b6fe849c5494cc6cc33288b7fb17eb3
mwpardue <mwpardue@gmail.com>
2022-06-14 20:44:31 -0400

    adding OSM, removing home-row mods

--- keyboards/crkbd/keymaps/caracarn/keymap.c
+++ keyboards/crkbd/keymaps/caracarn/keymap.c
@@ -13,9 +13,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT_split_3x6_3(
-  LLOCK,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    LLOCK,
-  ESC_CTL, KC_A,    CTL_S,   ALT_D,   GUI_F,   KC_G,                         KC_H,    GUI_J,   ALT_K,   CTL_L,   TD_QUOT, KC_SCLN,
-  TD_S15,  GUI_Z,   KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  LLOCK,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    OSMRALT,
+  OSMLCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    TD_QUOT, OSMLGUI,
+  OSMLALT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSMRCTL,
                                       OSMLGUI, TAB_NUM, CAP_KEY,    BSP_SYM, SPC_MAC, ENT_FUN
 ),
 
